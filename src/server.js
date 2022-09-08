4960a3541d6ad22cb330d70a5dfe9957a7f233a2
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-07 21:29:08 -0400

    server for express backend

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+const client = require("../db");
+const houseRouter = require("./houses/houses.router");
+const errorHandler = require("./errors/errorHandler");
+const pathNotFound = require("./errors/NotFound");
+const NotAllowed = require("./errors/NotAllowed");
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use("/houses", houseRouter);
+app.use(pathNotFound);
+app.use(errorHandler);
+
+
+app.listen(PORT, async () => {
+  try {
+    await client.connect();
+    console.log("Connected to database");
+    console.log(`Server is running on port ${PORT}`);
+  } catch (e) {
+    console.log(e);
+  }
+});
