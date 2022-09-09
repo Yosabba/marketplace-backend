a47cdf1c77f7fed51a54a46377f79aa3f977cfec
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-09 11:01:40 -0400

    too the .all method to check if it is allowed off the routes

--- src/houses/houses.router.js
+++ src/houses/houses.router.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const controller = require("./houses.controller");
-const notFound = require('../errors/NotAllowed');
+const notFound = require("../errors/NotAllowed");
 const NotAllowed = require("../errors/NotAllowed");
 
-router.route("/").get(controller.list).post(controller.create).all(NotAllowed);
+router.route("/").get(controller.list).post(controller.create);
 
-router.route("/:houseId").get(controller.read).put(controller.update).delete(controller.destroy).all(NotAllowed);
+router
+  .route("/:houseId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.destroy);
 
-
-module.exports = router
+module.exports = router;
