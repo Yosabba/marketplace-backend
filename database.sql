680276be51f4264c676557dd22c4dda05d977392
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-10 22:57:18 -0400

    :zap: created a table for users in my postgres database

--- database.sql
+++ database.sql
@@ -1,17 +1,24 @@
 
-INSERT INTO houses (bedroom, bathroom, house_location, furnished, price, offer, _description, parking, _type) VALUES (6, 3, 'norcross, GA', true, 500000, false, 'A very nice house in GA great for rental property', true, 'Rent');
+INSERT INTO houses (bedroom, bathroom, house_location, furnished, price, offer, _description, parking, _type, image_url) VALUES (6, 3, 'norcross, GA', true, 500000, false, 'A very nice house in GA great for rental property', true, 'Rent');
 
 CREATE TABLE houses (
     ID SERIAL PRIMARY KEY,
     bedroom INT NOT NULL,
     bathroom INT NOT NULL,
-    house_location VARCHAR(20),
-    furnished BOOLEAN,
+    house_location VARCHAR(20) NOT NULL,
+    furnished BOOLEAN NOT NULL,
     price INT NOT NULL,
-    offer BOOLEAN,
-    _description VARCHAR(100),
-    parking BOOLEAN,
-    _type VARCHAR(20),
+    offer BOOLEAN NOT NULL,
+    _description VARCHAR(100) NOT NULL,
+    parking BOOLEAN NOT NULL,
+    _type VARCHAR(20) NOT NULL,
     image_url VARCHAR(200)
+);
+
+CREATE TABLE users (
+    ID SERIAL PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    _password VARCHAR(20) NOT NULL,
+    email VARCHAR(20) NOT NULL
 );
 
