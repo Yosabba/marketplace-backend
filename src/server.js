f06defcb18a09f3c48610b60c751fd705064c1fc
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-12 13:44:11 -0400

    :fire: deleted not found and not allowed files

--- src/server.js
+++ src/server.js
@@ -6,14 +6,14 @@ const PORT = process.env.PORT || 5000;
 const client = require("../db");
 const houseRouter = require("./houses/houses.router");
 const errorHandler = require("./errors/errorHandler");
-const pathNotFound = require("./errors/NotFound");
-const NotAllowed = require("./errors/NotAllowed");
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
 app.use("/houses", houseRouter);
 app.use(errorHandler);
+
 app.listen(PORT, async () => {
   try {
     await client.connect();
