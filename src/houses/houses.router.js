0b058089864173947c80ead9dce482e6645104b1
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-12 13:46:15 -0400

    :fire: deleted not found

--- src/houses/houses.router.js
+++ src/houses/houses.router.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const controller = require("./houses.controller");
-const notFound = require("../errors/NotAllowed");
 const NotAllowed = require("../errors/NotAllowed");
 
 router.route("/").get(controller.list).post(controller.create);
