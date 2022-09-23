694eabe42d117c73f42fa8c72dbf06a1b993cb01
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-23 19:52:03 -0400

    added login route

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const client = require("../db");
 const houseRouter = require("./houses/houses.router");
+const loginRouter = require("./login/login.router")
 const errorHandler = require("./errors/errorHandler");
 
 app.use(morgan("dev"));
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/houses", houseRouter);
+app.use("/login", loginRouter)
 app.use(errorHandler);
 
 app.listen(PORT, async () => {
