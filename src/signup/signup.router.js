4aeb9cdbfd8831139f1722d7a6bcb7933585e982
Dominic Yosabba Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-24 14:14:58 -0400

    changed name of controller

--- src/signup/signup.router.js
+++ src/signup/signup.router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const controller = require("./signup.controller");
 
-router.route("/").post(controller.signUp);
+router.route("/").post(controller.signup);
 
 module.exports = router;
