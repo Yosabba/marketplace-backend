84d32433964a98c26164644e672ff14568d2e141
Dominic Yosabba Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-29 18:35:54 -0400

    update

--- src/login/login.controller.js
+++ src/login/login.controller.js
@@ -60,7 +60,7 @@ async function login(req, res, next) {
       maxAge: 1000 * 60 * 60 * 24,
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "None",
     }); //secure: true
 
     res.status(200).json({
