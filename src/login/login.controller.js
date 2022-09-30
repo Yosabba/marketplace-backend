85716920fe05e54404a37f4c94931f674308cd61
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-29 20:23:51 -0400

    added samesite strict

--- src/login/login.controller.js
+++ src/login/login.controller.js
@@ -59,6 +59,7 @@ async function login(req, res, next) {
     res.cookie("jwt", accessToken, {
       httpOnly: true,
       secure: true,
+      sameSite: "Strict",
       maxAge: 1000 * 60 * 60 * 24,
     }); //secure: true
 
