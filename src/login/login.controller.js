50e2154dc919ab0d373b36979f9a1d3f8d879fc0
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-30 12:14:54 -0400

    update

--- src/login/login.controller.js
+++ src/login/login.controller.js
@@ -59,7 +59,7 @@ async function login(req, res, next) {
     res.cookie("jwt", accessToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
+      sameSite: "None",
       maxAge: 1000 * 60 * 60 * 24,
     }); //secure: true
 
