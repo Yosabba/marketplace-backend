6effbfb0db5cb3e32d881cd34197de8ec274a924
Dominic Yosabba Johnson <85464248+Yosabba@users.noreply.github.com>
2022-09-30 13:34:06 -0400

    update same site value

--- src/login/login.controller.js
+++ src/login/login.controller.js
@@ -59,7 +59,7 @@ async function login(req, res, next) {
     res.cookie("jwt", accessToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "None",
+      sameSite: "lax",
       maxAge: 1000 * 60 * 60 * 24,
     }); //secure: true
 
